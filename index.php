e41c626587fec0a914fc939e93c677e56e7672b2
yuna-18 <huraigon10@gmail.com>
2024-12-11 22:34:16 +0900

    登録フォーム見た目作成

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,43 +8,58 @@
   <link rel="stylesheet" href="./css/reset.css">
   <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
+  <h1>ユーザー登録フォーム</h1>
   <div class="registration" id="registration">
-    <form action="" method="post">
-      <div class="input__outer">
-        <label for="name">氏名</label>
-        <input type="text" name="name" id="name">
-      </div>
-      <div class="input__outer">
-        <label for="furigana">フリガナ</label>
-        <input type="text" name="furigana" id="furigana">
-      </div>
-      <div class="input__outer">
-        <label for="email">メール</label>
-        <input type="text" name="email" id="email">
-      </div>
-      <div class="input__outer">
-        <p>好きな音楽のカテゴリ</p>
-        <input type="checkbox" name="orchestra" id="orchestra">
-        <label for="orchestra">オーケストラ</label>
-        <input type="checkbox" name="wind-orchestra" id="wind-orchestra">
-        <label for="wind-orchestra">吹奏楽</label>
-        <input type="checkbox" name="chamber-music-ensemble" id="chamber-music-ensemble">
-        <label for="chamber-music-ensemble">室内楽・アンサンブル</label>
-        <input type="checkbox" name="jazz" id="jazz">
-        <label for="jazz">ジャズ</label>
-        <input type="checkbox" name="solo" id="solo">
-        <label for="solo">ソロ</label>
-      </div>
-      <div class="input__outer">
-        <p>メールで演奏会の通知を受け取れます。</p>
-      <input type="checkbox" name="subscribe" id="subscribe">
-      <label for="subscribe">受け取る</label>
-      </div>
+    <form action="" method="post" class="form__container">
+        <div class="input__outer">
+          <label for="name">氏名</label>
+          <input type="text" name="name" id="name">
+        </div>
+        <div class="input__outer">
+          <label for="furigana">フリガナ</label>
+          <input type="text" name="furigana" id="furigana">
+        </div>
+        <div class="input__outer">
+          <label for="email">メール</label>
+          <input type="text" name="email" id="email">
+        </div>
+        <div class="input__outer">
+          <p class="question">好きな音楽のカテゴリ</p>
+          <ul class="input-check__list">
+            <li class="input-check__item">
+              <input type="checkbox" name="orchestra" id="orchestra">
+              <label for="orchestra">オーケストラ</label>
+            </li>
+            <li class="input-check__item">
+              <input type="checkbox" name="wind-orchestra" id="wind-orchestra">
+              <label for="wind-orchestra">吹奏楽</label>
+            </li>
+            <li class="input-check__item">
+              <input type="checkbox" name="chamber-music-ensemble" id="chamber-music-ensemble">
+              <label for="chamber-music-ensemble">室内楽・アンサンブル</label>
+            </li>
+            <li class="input-check__item">
+              <input type="checkbox" name="jazz" id="jazz">
+              <label for="jazz">ジャズ</label>
+            </li>
+            <li class="input-check__item">
+              <input type="checkbox" name="solo" id="solo">
+              <label for="solo">ソロ</label>
+            </li>
+          </ul>
+        </div>
+        <div class="input__outer">
+        <p class="question">メールで演奏会の通知を受け取れます。</p>
+        <div class="input-check__item">
+          <input type="checkbox" name="subscribe" id="subscribe">
+          <label for="subscribe">受け取る</label>
+        </div>
+        </div>
     </form>
-    <button type="submit">確認する</button>
-  </div>
-  
+    <a class="confirm-btn" href="./php/confirm.php">確認する</a>
   </div>
 </body>
+
 </html>
