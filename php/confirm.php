573f4b5c193aebd9a836d612d4beb314e612d326
yuna-18 <huraigon10@gmail.com>
2024-12-11 19:47:28 +0900

    フォーム作成中

---
+++ php/confirm.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ユーザー登録フォーム</title>
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<div id="confirm" class="confirm">
+    <div class="confirm__outer">
+      <p>氏名</p>
+      
+    </div>
+    <div class="confirm__outer">
+      <p>フリガナ</p>
+      
+    </div>
+    <div class="confirm__outer">
+      <p>メール</p>
+      
+    </div>
+    <div class="confirm__outer">
+      <p>好きな音楽のカテゴリ</p>
+      
+    </div>
+    <div class="confirm__outer">
+      <p>メールで演奏会の通知を受け取る</p>
+      
+    </div>
+</body>
+</html>
